6ad731a10a0443f72461409f68803886a807579a
Nevillested <olegsidnev@gmail.com>
2022-05-14 00:24:53 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            XNamespace ya = "http://weather.yandex.ru/forecast";
+            var t = XDocument.Load("http://export.yandex.ru/weather-ng/forecasts/29000.xml");
+            var days = t.Document.Root.Elements(ya + "day");
+            foreach (var day in days)
+            {
+                Console.WriteLine(day.Attribute("date").Value);
+                Console.WriteLine(day.Element(ya + "sunrise").Value + " Восход");
+                Console.WriteLine(day.Element(ya + "sunset").Value + " Закат");
+                Console.WriteLine();
+            }
+        }
+    }
+}
